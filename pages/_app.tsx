ece832003ecc91f85508bce72791dc4ba5d937f9
npkhoa2197 <khoa.nguyen@hawking.network>
2021-06-05 21:26:22 +0700

    Fix typo http-equiv -> httpEquiv

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,7 +38,7 @@ const MyApp = ({ Component, pageProps }) => {
     <>
       {/* Favicon */}
       <Head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
