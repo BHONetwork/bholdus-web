6643e6462fbf7579da49cce833f17ede5a18944e
npkhoa2197 <khoa.nguyen@hawking.network>
2021-06-06 17:18:15 +0700

    Update background of landing page hero and fix bug in intro video

--- components/sections/introduction.tsx
+++ components/sections/introduction.tsx
@@ -27,45 +27,47 @@ const Introduction = ({ data }) => {
   const { t } = useTranslation();
 
   return (
-    <section
-      className="flex flex-col lg:flex-row justify-end flex-shrink mt-40 md:mt-0"
-      data-aos="zoom-in-left"
-    >
-      <div
-        className="lg:relative flex justify-center items-center max-h-min lg:mb-0 -mb-16 z-10"
-        style={{ maxHeight: 205 }}
+    <>
+      <section
+        className="flex flex-col lg:flex-row justify-end flex-shrink mt-40 md:mt-0"
+        data-aos="zoom-in-left"
       >
-        {data.introductionVideoLink && (
-          <PlayButton onClick={() => setDisplayIntroductionVideo(true)} />
-        )}
-      </div>
-      <GreenBackground className="max-w-4xl p-10 md:pt-20 md:pb-20 md:pr-24 md:pl-28">
-        <div className="flex flex-row items-center mb-12">
-          <div
-            className="mr-2"
-            style={{ backgroundColor: "white", width: 40, height: 1 }}
-          />
-          <Text size="small" weight="bold" uppercase>
-            {t("common:introduction")}
-          </Text>
+        <div
+          className="lg:relative flex justify-center items-center max-h-min lg:mb-0 -mb-16 z-10"
+          style={{ maxHeight: 205 }}
+        >
+          {data.introductionVideoLink && (
+            <PlayButton onClick={() => setDisplayIntroductionVideo(true)} />
+          )}
         </div>
-        <Text className="mb-10" type="h2">
-          {data.title}
-        </Text>
-        <Text className="mb-10" type="p">
-          {data.description}
-        </Text>
-        <ul className="pl-6">
-          {data.featuredPoints.map((point: any) => (
-            <li className="flex flex-row" key={point.id}>
-              <div className="mr-2" style={{ color: "white" }}>
-                •
-              </div>
-              <Text type="p">{point.content}</Text>
-            </li>
-          ))}
-        </ul>
-      </GreenBackground>
+        <GreenBackground className="max-w-4xl p-10 md:pt-20 md:pb-20 md:pr-24 md:pl-28">
+          <div className="flex flex-row items-center mb-12">
+            <div
+              className="mr-2"
+              style={{ backgroundColor: "white", width: 40, height: 1 }}
+            />
+            <Text size="small" weight="bold" uppercase>
+              {t("common:introduction")}
+            </Text>
+          </div>
+          <Text className="mb-10" type="h2">
+            {data.title}
+          </Text>
+          <Text className="mb-10" type="p">
+            {data.description}
+          </Text>
+          <ul className="pl-6">
+            {data.featuredPoints.map((point: any) => (
+              <li className="flex flex-row" key={point.id}>
+                <div className="mr-2" style={{ color: "white" }}>
+                  •
+                </div>
+                <Text type="p">{point.content}</Text>
+              </li>
+            ))}
+          </ul>
+        </GreenBackground>
+      </section>
       {data.introductionVideoLink && (
         <VideoModal
           isShown={displayIntroductionVideo}
@@ -73,7 +75,7 @@ const Introduction = ({ data }) => {
           src={data.introductionVideoLink}
         />
       )}
-    </section>
+    </>
   );
 };
 
