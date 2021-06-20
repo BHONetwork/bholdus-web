f18e28fb2f1c1d954750e75175968100e11aa9e9
naviocean <navi.ocean@outlook.com>
2021-06-20 12:19:16 +0700

    fix favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -57,7 +57,25 @@ const MyApp = ({ Component, pageProps }) => {
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         {/* Favicon */}
-        <link rel="shortcut icon" href={getMediaUrl(global.favicon.url)} />
+        {/* <link rel="shortcut icon" href={getMediaUrl(global.favicon.url)} /> */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       {/* Global site metadata. Acts as default SEO, can be overriden on a page by page basis if needed */}
