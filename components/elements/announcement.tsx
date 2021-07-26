10911b763f66e0f74b96745c0d2c99a04335b513
Duong Le <navi.ocean@outlook.com>
2021-07-26 16:28:14 +0700

    Add close button for Announcement

--- components/elements/announcement.tsx
+++ components/elements/announcement.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import classNames from "classnames";
 
 import OptimizedImage from "../common/optimized-image";
 const Announcement = ({ data, show }) => {
+  const [close, setClose] = useState(false);
+
   if (data?.enable && show)
     return (
       <div
-        className={classNames("announcement")}
+        className={classNames("announcement", { hidden: close })}
         style={{ marginLeft: -(data.width / 2) }}
       >
         <div className="container">
           <div className="announcement-container">
+            <button
+              className="announcement-close"
+              onClick={() => setClose(true)}
+            >
+              <OptimizedImage
+                img={{
+                  url: "/images/close.svg",
+                  alternativeText: "play-button",
+                }}
+                width={25}
+                height={25}
+              />
+            </button>
             <div className="announcement-image">
               <a
                 key={data.id}
