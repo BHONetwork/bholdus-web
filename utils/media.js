94103276d6017cacf952d723b477111f1e349025
Duong Le <navi.ocean@outlook.com>
2021-08-03 10:44:38 +0700

    Using cloudfront cdn.bholdu.com

--- utils/media.js
+++ utils/media.js
@@ -4,5 +4,9 @@ import _ from "lodash";
 export function getMediaUrl(media) {
   let imageUrl = _.isString(media) ? media : media.url;
   imageUrl = imageUrl.startsWith("/") ? getBackendUrl(imageUrl) : imageUrl;
+  imageUrl = imageUrl.replace(
+    "bholdus.s3.ap-southeast-1.amazonaws.com",
+    "cdn.bholdus.com"
+  );
   return imageUrl;
 }
