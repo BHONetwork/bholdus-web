72681b6eaf9936fbdb85e62fd463d54ef019e177
Duong Le <navi.ocean@outlook.com>
2021-08-15 15:01:37 +0700

    Update parser markdown

--- components/common/rich-text/index.tsx
+++ components/common/rich-text/index.tsx
@@ -1,21 +1,9 @@
-import classNames from "classnames";
-import ReactMarkdown from "react-markdown";
-import Image from "./image";
-import Paragraph from "./paragraph";
+import MarkDown from "./markdown";
 
 import { RichTextProps } from "./types";
 
 const RichText = (props: RichTextProps) => {
-  const { className = "", skipHtml = false, ...restProps } = props;
-  return (
-    <ReactMarkdown
-      className={classNames("markdown-body", className)}
-      linkTarget="_blank"
-      skipHtml={skipHtml}
-      components={{ img: Image, p: Paragraph }}
-      {...restProps}
-    />
-  );
+  return <MarkDown {...props} />;
 };
 
 export default RichText;
