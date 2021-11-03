519a0202cb94777558815c5102447f7a93ae7ad9
Duong Le <navi.ocean@outlook.com>
2021-11-03 20:59:04 +0700

    Add infobip chat

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,6 +42,18 @@ class MyDocument extends Document {
           `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(I,n,f,o,b,i,p){I[b]=I[b]||function(){(I[b].q=I[b].q||[]).push(arguments)};
+            I[b].t=1*new Date();i=n.createElement(f);i.async=1;i.src=o;
+            p=n.getElementsByTagName(f)[0];p.parentNode.insertBefore(i,p)})
+            (window,document,'script','https://livechat.infobip.com/widget.js','liveChat');
+            
+            liveChat('init', '9807e206-fc76-45fa-ad55-e9fbabe8ed9c');
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
