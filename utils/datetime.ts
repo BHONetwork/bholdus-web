44fb53c72ec74b35f8beeac99235cf36ef03263f
Duong Le <navi.ocean@outlook.com>
2022-02-19 14:57:35 +0700

    Update date locale format

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -4,5 +4,6 @@ import "dayjs/locale/en";
 import "dayjs/locale/vi";
 
 export const formatDate = (lang: string, date: string): string => {
+  if (lang === "vi") return dayjs(date).locale(lang).format("DD MMMM, YYYY");
   return dayjs(date).locale(lang).format("MMMM DD, YYYY");
 };
